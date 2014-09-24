e1560baf9dd98ac968ed3c32fff5b682faab6cd9
Denis Stepulenok <super.denis@gmail.com>
2014-09-24 22:17:27 +0400

    Обобщённый код (используем параметр T - Generic)

--- src/main/java/dao/Repository.java
+++ src/main/java/dao/Repository.java
@@ -1,8 +1,25 @@
 package dao;
 
 /**
+ * Репозиторий
  *
+ * T - класс, объекты которого будем хранить в репозитории
  */
-public interface Repository {
-    int insert(Entity obj) throws Exception;
+public interface Repository<T extends Entity> {
+    /**
+     * Добавление в таблицу
+     *
+     * @param obj Объект, который нужно добавить
+     * @return Новый идентификатор в хранилище
+     * @throws Exception
+     */
+    int insert(T obj) throws Exception;
+
+    /**
+     * Получение объекта по идентификатору
+     *
+     * @param id идентификатор
+     * @return Объект
+     */
+    T get(int id);
 }
--- src/test/java/examples/xml/MyClassXMLTest.java
+++ src/test/java/examples/xml/MyClassXMLTest.java
@@ -1,9 +1,14 @@
 package examples.xml;
 
+import dao.Repository;
+import dao.XMLFileRepository;
+import model.User;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Тестируем сохранение и загрузку
@@ -16,7 +21,8 @@ public class MyClassXMLTest extends Assert {
     public void testSaveLoad() throws Exception {
         MyClass myClass = new MyClass();
         myClass.setName("Петя");
-        MyClassHelper.write(myClass, FILENAME);
+        myClass.setAge(22);
+        XMLHelper.write(myClass, FILENAME);
 
         File f = new File(FILENAME);
         assertTrue(f.exists());
@@ -24,8 +30,46 @@ public class MyClassXMLTest extends Assert {
         assertTrue(new File(FILENAME).isFile());
 
         // Считываем обратно
-        MyClass f2 = MyClassHelper.read(FILENAME);
+        MyClass f2 = (MyClass) XMLHelper.read(FILENAME);
         System.out.println("Прочитали: " + f2.getName());
+    }
+
+    @Test
+    public void testSaveLoadArrayOfObjects() throws Exception {
+        List<User> list = new ArrayList<User>();
+        list.add(new User("User1"));
+        list.add(new User("User2"));
+        list.add(new User("User3"));
+
+        XMLHelper.write(list, "list.xml");
+
+        List<User> newList = (ArrayList<User>) XMLHelper.read("list.xml");
+
+        assertEquals("User1", newList.get(0).getUsername());
+        assertEquals("User2", newList.get(1).getUsername());
+        assertEquals("User3", newList.get(2).getUsername());
+    }
+
+    @Test
+    public void testSaveLoadRepositoryObjects() throws Exception {
+        XMLFileRepository xmlFileRepository =
+                new XMLFileRepository("users.xml");
+
+        Repository<User> repository = xmlFileRepository;
+        repository.insert(new User("User1"));
+        repository.insert(new User("User2"));
+        repository.insert(new User("User3"));
+
+        xmlFileRepository.save();
+
+        XMLFileRepository newXmlFileRepository = new XMLFileRepository("users.xml");
+        newXmlFileRepository.load();
+
+        Repository<User> rep = newXmlFileRepository;
+
+        assertEquals("User1", rep.get(0).getUsername());
+        assertEquals("User2", rep.get(0).getUsername());
+        assertEquals("User2", rep.get(0).getUsername());
     }
 
 
