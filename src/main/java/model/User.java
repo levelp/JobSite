af631a20fb32b798b7f9fbe6bfba8a8b74e26982
Denis Stepulenok <super.denis@gmail.com>
2014-09-24 20:10:25 +0400

    Начальная структура проекта

---
+++ src/main/java/model/User.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * Пользователь
+ */
+public class User {
+    public int id;
+
+    public User() {
+        // TODO: получить новый идентификатор пользователя
+        id = 1;
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,17 @@
+import model.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Базовые операции с пользователями
+ */
+public class UserTest extends Assert {
+    /**
+     * Создание нового пользователя
+     */
+    @Test
+    public void createNewUser() {
+        User user = new User();
+        assertEquals(1, user.id);
+    }
+}
