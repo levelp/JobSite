2f420adc9df46e7a94fee76f100b167dfeaa082b
Denis Stepulenok <super.denis@gmail.com>
2014-09-24 22:13:09 +0400

    Добавлено имя пользователя

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -9,10 +9,23 @@ public class User implements Entity {
     public int id;
 
     private String email;
+    private String username;
 
     public User() {
         // TODO: получить новый идентификатор пользователя
         id = 1;
+    }
+
+    public User(String username) {
+        setUsername(username);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
