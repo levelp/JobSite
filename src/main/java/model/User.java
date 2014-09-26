879de3cf4f18b573e0a52e65dcf966658c98b8c0
Denis Stepulenok <super.denis@gmail.com>
2014-09-26 22:13:59 +0400

    Проверка пароля

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -8,11 +8,24 @@ import java.util.regex.Pattern;
  * Пользователь
  */
 public class User implements Entity {
+    public static final int MIN_PASSWORD_LENGTH = 8;
+    public static final String PASSWORD_REQUIREMENTS =
+            "Пароль должен быть не меньше 8 символов, содержать цифры, заглавные и строчные буквы и не совпадал" +
+                    " с именем пользователя";
+
     public static final Pattern emailPattern = Pattern.compile("(\\w+.)*\\w+@(\\w+.)+\\w+");
+    public static final String PASSWORD_NOT_CONTAIN_UPPERCASE_AND_DIGITS =
+            "Пароль не содержит заглавных букв и цифр";
     public int id;
 
     private String email;
     private String username;
+
+    // Новый пароль при регистрации (не хранится в БД)
+    private String newPassword;
+
+    // Зашифрованный пароль (как он хранится в БД)
+    private String passwordHash;
 
     public User() {
         // TODO: получить новый идентификатор пользователя
@@ -60,5 +73,46 @@ public class User implements Entity {
     @Override
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String password) {
+
+        this.newPassword = password;
+    }
+
+    /**
+     * Проверка пароля на сложность
+     *
+     * @return
+     */
+    public String checkPasswordComplexity() {
+        if (newPassword.length() < MIN_PASSWORD_LENGTH)
+            return "Пароль слишком короткий, должен быть не менее 8 символов";
+        int lowerLetter = 0;
+        int upperLetter = 0;
+        int digits = 0;
+        for (int i = 0; i < newPassword.length(); ++i) {
+            char c = newPassword.charAt(i);
+            if (Character.isDigit(c))
+                digits++;
+            if (Character.isLowerCase(c))
+                lowerLetter++;
+            if (Character.isUpperCase(c))
+                upperLetter++;
+        }
+        if (upperLetter == 0 && digits == 0)
+            return PASSWORD_NOT_CONTAIN_UPPERCASE_AND_DIGITS;
+        if (lowerLetter == 0 && digits == 0)
+            return "Пароль не содержит строчных букв и цифр";
+        if (digits == 0)
+            return "Пароль не содержит цифр";
+        if (upperLetter == 0 && lowerLetter == 0)
+            return "Пароль состоит только из цифр";
+
+        return null;
     }
 }
