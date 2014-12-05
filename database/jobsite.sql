f346ecbf38e0526c412c9e2eaf90e2653a7cfca1
Denis Stepulenok <super.denis@gmail.com>
2014-12-05 20:35:40 +0300

    Тестовые данные

--- database/jobsite.sql
+++ database/jobsite.sql
@@ -19,6 +19,18 @@ CREATE UNIQUE INDEX unique_email ON "user" USING BTREE (email);
 -- Добавляем тестовые данные
 INSERT INTO "user" (name, email, password, sex)
 VALUES ('Иванов Иван Иванович', 'test@mail.ru', '111', 'MALE');
+INSERT INTO "user" (name, email, password, sex)
+VALUES ('Петров Пётр Петрович', 'petrov@mail.ru', '444', 'MALE');
+INSERT INTO "user" (name, email, password, sex)
+VALUES ('Петров Иван', 'pivan@mail.ru', '11', 'MALE');
+INSERT INTO "user" (name, email, password, sex)
+VALUES ('Иванов Пётр', 'dfd@gmail.com', '32', 'MALE');
+INSERT INTO "user" (name, email, password, sex)
+VALUES ('Чубайс Пётр Иванович', 'mail1@gmail.com', '32', 'MALE');
+INSERT INTO "user" (name, email, password, sex)
+VALUES ('Иванов Пётр Иванович', 'mail1@gmail.com', '32', 'MALE');
+INSERT INTO "user" (name, email, password, sex)
+VALUES ('Путин Пётр Иванович', 'putin@gmail.com', '32', 'MALE');
 
 -- Создаём таблицу resume
 CREATE TABLE resume (
